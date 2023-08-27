c9f5c2c5adcacfc84912becbfcd283a77aafaeb7
Gauransh Juneja <gauranshjuneja@gmail.com>
2023-08-27 19:27:08 +0530

    first commit

---
+++ HTML,CSS,JAVASCRIPT/JavaScript/index.js
@@ -0,0 +1,12 @@
+var a = 10;
+function displayValue() {
+    var a = 100;
+    return a
+}
+function displayValue2() {
+    var a = 1000;
+    return a
+}
+displayValue()
+displayValue2();
+console.log(a)
